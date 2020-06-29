5a1b6305dd71074b568dcb4f69950fb9407c999e
Adeel <adeel@wfgames.co.uk>
2020-06-29 23:31:27 +0100

    Updates

---
+++ precache-manifest.d3b5ed10ff34f12624f61088d428cb40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bcf1947c1e773b6991cb9826e417bd",
+    "url": "/weatherpwa/index.html"
+  },
+  {
+    "revision": "48778e84045193a19bea",
+    "url": "/weatherpwa/static/css/main.88dd88a2.chunk.css"
+  },
+  {
+    "revision": "f5b280e2bd8dadc55dc2",
+    "url": "/weatherpwa/static/js/2.67ed364d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weatherpwa/static/js/2.67ed364d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48778e84045193a19bea",
+    "url": "/weatherpwa/static/js/main.4dc7fbdc.chunk.js"
+  },
+  {
+    "revision": "1c0c372cf0f66fce82c0",
+    "url": "/weatherpwa/static/js/runtime-main.22a50f92.js"
+  }
+]);
